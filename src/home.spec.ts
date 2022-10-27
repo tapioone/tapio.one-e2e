49d480c1c63ce40b6219de4b17c7c8f3983366a6
IO <io@develup.pro>
2022-10-27 14:48:24 +0200

    fix: refactor by adding page object, add beforeEach method. #5

--- src/home.spec.ts
+++ src/home.spec.ts
@@ -1,190 +1,197 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('Home Page has correct title and can open shop', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
+const {PageObject} = require('./page-object');
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+test.beforeEach(async ({page}) => {
+    const pageObject = new PageObject(page);
+    await pageObject.goto();
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Home | tapio /);
+test('Home Page has correct title and can open shop', async ({page, context}) => {
+    const pageObject = new PageObject(page);
 
-  // create a locator
-  const shopLink = page.getByText('Shop').nth(1);
+    // accept cookies
+    await pageObject.acceptCookies();
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(shopLink).toHaveAttribute('href', /https:\/\/customerportal.tapio.one\/marketplace/);
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Home | tapio /);
 
-  // Click the get started link. this will open a new tab
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    shopLink.click(),
-  ]);
+    // create a locator
+    const shopLink = pageObject.linkShop;
 
-  // Expects the URL to contain intro.
-  await expect(newPage).toHaveURL(/https:\/\/customerportal.tapio.one\/marketplace/);
+    // Expect an attribute "to be strictly equal" to the value.
+    await expect(shopLink).toHaveAttribute('href', /https:\/\/customerportal.tapio.one\/marketplace/);
+
+    // Click the get started link. this will open a new tab
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        shopLink.click(),
+    ]);
+
+    // Expects the URL to contain intro.
+    await expect(newPage).toHaveURL(/https:\/\/customerportal.tapio.one\/marketplace/);
 });
 
 
-test('Register Button in nav menu should lead to https://my.tapio.one/signup/landing?register=customer in a new tab', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
+test('Register Button in nav menu should lead to https://my.tapio.one/signup/landing?register=customer in a new tab',
+    async ({page, context}) => {
+        const pageObject = new PageObject(page);
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+        // accept cookies
+        await pageObject.acceptCookies();
 
-  // Click the 'My tapio' link. this will open a dropdown menu
-  await page.locator('.nav-link', { hasText: 'My tapio' }).click();
+        // Click the 'My tapio' link. this will open a dropdown menu
+        await pageObject.openMyTapioDropdown();
 
-  // Click the 'Register' link. this will open a new tab
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    page.locator('.dropdown-item', { hasText: /(Registrieren)|(Register)/ }).click()
-  ])
+        // Click the 'Register' link. this will open a new tab
+        const [newPage] = await Promise.all([
+            context.waitForEvent('page'),
+            pageObject.pressRegistrationButton()
+        ])
 
-  // Expects the URL to contain intro.
-  await expect(newPage).toHaveURL('https://my.tapio.one/signup/landing?register=customer');
+        // Expects the URL to contain intro.
+        await expect(newPage).toHaveURL('https://my.tapio.one/signup/landing?register=customer');
+    });
+
+
+test('Register Button in footer should lead to https://my.tapio.one/signup/landing?register=customer in a new tab',
+    async ({page, context}) => {
+
+        const pageObject = new PageObject(page);
+
+        // accept cookies
+        await pageObject.acceptCookies();
+
+        // Click the 'Register' link. this will open a new tab
+        const [newPage] = await Promise.all([
+            context.waitForEvent('page'),
+            page.click('.call-to-action')
+        ])
+
+        // Expects the URL to contain intro.
+        await expect(newPage).toHaveURL('https://my.tapio.one/signup/landing?register=customer');
+    });
+
+test('Switching the language should work', async ({page}) => {
+    const pageObject = new PageObject(page);
+
+    // accept cookies
+    await pageObject.acceptCookies();
+
+    // Click the 'My tapio' link. this will open a dropdown menu
+    await pageObject.openLanguageDropdown();
+
+    // Click the button with text 'DE'
+    await pageObject.switchToDeutsch();
+
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL('https://www.tapio.one/de/');
+
+    // Click the 'My tapio' link. this will open a dropdown menu
+    await pageObject.openLanguageDropdown();
+
+    // Click the button with text 'EN'
+    await pageObject.switchToEnglish();
+
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL('https://www.tapio.one/en/');
 });
 
 
-test('Register Button in footer should lead to https://my.tapio.one/signup/landing?register=customer in a new tab', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
+test('Scroll to Top should work', async ({page}) => {
+    const pageObject = new PageObject(page);
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+    // accept cookies
+    await pageObject.acceptCookies();
 
-  // Click the 'Register' link. this will open a new tab
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    page.click('.call-to-action')
-  ])
+    // find footer box
+    const footerBox = await pageObject.footer.boundingBox();
 
-  // Expects the URL to contain intro.
-  await expect(newPage).toHaveURL('https://my.tapio.one/signup/landing?register=customer');
+    await page.waitForTimeout(1000)
+
+    //scroll to the footer
+    if (footerBox) {
+        const y = footerBox.y;
+        await page.mouse.wheel(0, y)
+    }
+
+    await page.waitForTimeout(1000)
+
+    // click button 'to Top'
+    await pageObject.scrollToTop();
+
+    await page.waitForTimeout(1000)
+
+    // find header box
+    const headerBox = await pageObject.header.boundingBox();
+
+    if (headerBox) {
+        const headerY = headerBox.y
+        expect(headerY).toBe(0)
+    }
 });
 
 
-test('Switching the language should work', async ({ page }) => {
-  await page.goto('https://www.tapio.one/en/');
+test.skip('Switching between monthly/yearly billing should change the price', async ({page, context}) => {
+    // accept cookies
+    await page.locator("data-testid=uc-accept-all-button").click();
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Home | tapio /);
 
-  // Click the 'My tapio' link. this will open a dropdown menu
-  await page.locator('.nav-link', { hasText: 'language' }).click();
+    // create a locator
+    const shopLink = page.getByText('Shop').nth(1);
 
-  // Click the button with text 'DE'
-  await page.locator('button', { hasText: 'DE' }).click();
+    // Expect an attribute "to be strictly equal" to the value.
+    await expect(shopLink).toHaveAttribute('href', /https:\/\/customerportal.tapio.one\/marketplace/);
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL('https://www.tapio.one/de/');
+    // Click the get started link. this will open a new tab
+    const [shopPage] = await Promise.all([
+        context.waitForEvent('page'),
+        shopLink.click(),
+    ]);
 
-  // Click the 'My tapio' link. this will open a dropdown menu
-  await page.locator('.nav-link', { hasText: 'language' }).click();
+    // Expects the URL to contain intro.
+    await expect(shopPage).toHaveURL(/https:\/\/customerportal.tapio.one\/marketplace/);
 
-  // Click the button with text 'EN'
-  await page.locator('button', { hasText: 'EN' }).click();
+    await page.locator('#cookies-banner-accept-all').click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL('https://www.tapio.one/en/');
+    // I can't interact with this landing page =(
+    await page.click('button.buynow >> nth=0');
+    await page.locator('.PromotionTile').nth(0).click();
 });
 
 
-test('Scroll to Top should work', async ({ page }) => {
-  await page.goto('https://www.tapio.one/en/');
+test('When saving the default state, Google Analytics should be disabled', async ({page}) => {
+    const pageObject = new PageObject(page);
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+    // save services
+    await pageObject.setDefaultCookies();
 
-  await page.screenshot()
+    //expect no google-analytics script
+    await expect(pageObject.analyticScript).toHaveCount(0)
 
-  // find footer box
-  const footerBox = await page.locator('footer').boundingBox();
+    // get quantity of cookies
+    const cookiesArrayLength = await (await pageObject.cookies).length
 
-  //scroll to the footer
-  if (footerBox) {
-    const y = footerBox.y;
-    await page.mouse.wheel(0, y)
-  }
-
-  await page.waitForTimeout(1000)
-
-  // click button 'to Top'
-  await page.locator('.backToTop__ScrollToTopButton-mdwnp9-0').click()
-
-  await page.waitForTimeout(1000)
-
-  // find header box
-  const headerBox = await page.locator('.header').boundingBox();
-
-  if (headerBox) {
-    const headerY = headerBox.y
-    expect(headerY).toBe(0)
-  }
+    // expect no cookies
+    await expect(cookiesArrayLength).toBe(0)
 });
 
 
-test.skip('Switching between monthly/yearly billing should change the price', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
+test('When declining all cookies no scripts should be loaded', async ({page}) => {
+    const pageObject = new PageObject(page);
 
-  // accept cookies 
-  await page.locator("data-testid=uc-accept-all-button").click();
+    // deny cookies
+    await pageObject.denyCookies()
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Home | tapio /);
+    //expect no google-analytics script
+    await expect(pageObject.analyticScript).toHaveCount(0)
 
-  // create a locator
-  const shopLink = page.getByText('Shop').nth(1);
+    // get quantity of cookies
+    const cookiesArrayLength = await (await pageObject.cookies).length
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(shopLink).toHaveAttribute('href', /https:\/\/customerportal.tapio.one\/marketplace/);
-
-  // Click the get started link. this will open a new tab
-  const [shopPage] = await Promise.all([
-    context.waitForEvent('page'),
-    shopLink.click(),
-  ]);
-
-  // Expects the URL to contain intro.
-  await expect(shopPage).toHaveURL(/https:\/\/customerportal.tapio.one\/marketplace/);
-
-  await page.locator('#cookies-banner-accept-all').click();
-
-  // I can't interact with this landing page =(
-  await page.click('button.buynow >> nth=0');
-  await page.locator('.PromotionTile').nth(0).click();
-});
-
-
-test('When saving the default state, Google Analytics should be disabled', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
-
-  // save services
-  await page.locator("data-testid=uc-save-button").click();
-
-  //expect no google-analytics script
-  await expect(page.locator('script[src="https://www.google-analytics.com/analytics.js"]')).toHaveCount(0)
-
-  // get quantity of cookies
-  const cookiesArrayLength = await (await context.cookies()).length
-
-  // expect no cookies
-  await expect(cookiesArrayLength).toBe(0)
-});
-
-
-test('When declining all cookies no scripts should be loaded', async ({ page, context }) => {
-  await page.goto('https://www.tapio.one/');
-
-  // deny cookies 
-  await page.locator("data-testid=uc-deny-all-button").click();
-
-  //expect no google-analytics script
-  await expect(page.locator('script[src="https://www.google-analytics.com/analytics.js"]')).toHaveCount(0)
-
-  // get quantity of cookies
-  const cookiesArrayLength = await (await context.cookies()).length
-
-  // expect no cookies
-  await expect(cookiesArrayLength).toBe(0)
+    // expect no cookies
+    await expect(cookiesArrayLength).toBe(0)
 });
 
